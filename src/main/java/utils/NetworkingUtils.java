2b2deb9302198252f20772a1efbadd994e63aa0e
Vanish <blazejwojcik008@gmail.com>
2023-04-02 13:04:37 +0200

    xD

--- src/main/java/utils/NetworkingUtils.java
+++ src/main/java/utils/NetworkingUtils.java
@@ -19,29 +19,28 @@ public class NetworkingUtils {
         }
     }
 
-    public static void download(String link, String target){
-        try(InputStream in = new URL(link).openStream()){
+    public static void download(String link, String target) {
+        try (InputStream in = new URL(link).openStream()) {
             Files.copy(in, Paths.get(target));
             Download.setDone(true);
-            if(new File(target).exists())
+            if (new File(target).exists())
                 Download.setFileExists(true);
 
             complete(target);
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void complete(String target){
+    public static void complete(String target) {
         System.out.println("Download complete! \nDo you want to set this image as a wallpaper? (Y/N): ");
         Scanner scanner = new Scanner(System.in);
-        switch (scanner.next().toLowerCase()){
+        switch (scanner.next().toLowerCase()) {
             case "y":
                 SystemHelper.setWallpaper(target);
                 break;
             case "n":
                 break;
-
         }
     }
 }
--- src/main/java/utils/SystemHelper.java
+++ src/main/java/utils/SystemHelper.java
@@ -1,8 +1,21 @@
 package utils;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
 public class SystemHelper {
 
-    public static void setWallpaper(String image){
+    public static void setWallpaper(String image) throws IOException {
+        File wallpaper = new File(image);
+        Image img = ImageIO.read(wallpaper);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
+        // skalujemy obrazek do rozmiarow ekranu (zeby sie dobrze wygladalo)
+        Image resizedImg = img.getScaledInstance(screenSize.width, screenSize.height, Image.SCALE_SMOOTH);
+
+        // ustawiamy tapete
+        WallpaperSetter.set(resizedImg);
     }
 }
