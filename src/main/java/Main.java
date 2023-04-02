69ad6705b02b8c2510ebee5bbcd3693486d12347
Sussy Baka <63926031+SussyDeveloper@users.noreply.github.com>
2023-04-02 11:11:09 +0200

    changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,8 @@
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
 public class Main {
 
     public static void main(String[] args){
 
         System.out.println("Choose website do you want have a random image: \n[1] astolfo.rocks\n[2] waifu.pics");
 
-        new KeyListener(){
-
-            @Override
-            public void keyTyped(KeyEvent e) {
-
-            }
-
-            @Override
-            public void keyPressed(KeyEvent e) {
-                switch (e.getKeyCode()){
-                    case 1:
-                        break;
-                }
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {
-
-            }
-        };
     }
 }
